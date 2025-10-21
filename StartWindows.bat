82912bb0596b3c8f5c8d5cd48ec71eb5f4871678
Leonardo Feitosa <leonardo.sfeitosa@hotmail.com>
2025-10-21 17:54:38 -0300

    Improving the .bat

--- StartWindows.bat
+++ StartWindows.bat
@@ -1,27 +1,26 @@
 @echo off
 setlocal
-
+set "PROJ=%~dp0LegalQuorum\LegalQuorum.csproj"
 set "PUB=%~dp0LegalQuorum\bin\Release\net8.0\win-x64\publish"
 set "APP=%PUB%\LegalQuorum.exe"
 
 if not exist "%APP%" (
-  echo [ERRO] Nao encontrei o executavel: %APP%
-  pause
-  exit /b 1
+  echo [INFO] Not Founded. Publishing...
+  dotnet publish "%PROJ%" -c Release -r win-x64 -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true || (
+    echo [ERRO] Fail at publish.
+    pause
+    exit /b 1
+  )
 )
 
 set "ASPNETCORE_CONTENTROOT=%PUB%"
-
-set "PORT=5000"
-
-start "" /D "%PUB%" "%APP%" --urls=http://localhost:%PORT%
+start "" /D "%PUB%" "%APP%" --urls=http://localhost:5000"
 
 for /l %%i in (1,1,15) do (
-  powershell -NoProfile -Command "try{(New-Object Net.Sockets.TcpClient 'localhost',%PORT%).Dispose();exit 0}catch{exit 1}" >nul
+  powershell -NoProfile -Command "try{(New-Object Net.Sockets.TcpClient 'localhost',5000).Dispose();exit 0}catch{exit 1}" >nul
   if not errorlevel 1 goto :open
   timeout /t 1 >nul
 )
-
 :open
-start "" "http://localhost:%PORT%"
+start "" "http://localhost:5000"
 endlocal
