bbfa3497ca3d37f1ba0d30c5f04a501fc35a531f
Leonardo Feitosa <leonardo.sfeitosa@hotmail.com>
2025-10-21 18:08:01 -0300

    Improving the bat to publish

--- StartWindows.bat
+++ StartWindows.bat
@@ -1,26 +1,29 @@
 @echo off
 setlocal
-set "PROJ=%~dp0LegalQuorum\LegalQuorum.csproj"
-set "PUB=%~dp0LegalQuorum\bin\Release\net8.0\win-x64\publish"
+set "ROOT=%~dp0"
+set "PROJ=%ROOT%LegalQuorum\LegalQuorum.csproj"
+set "PUB=%ROOT%LegalQuorum\bin\Release\net8.0\win-x64\publish"
 set "APP=%PUB%\LegalQuorum.exe"
+set "PORT=5000"
 
 if not exist "%APP%" (
   echo [INFO] Not Founded. Publishing...
-  dotnet publish "%PROJ%" -c Release -r win-x64 -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true || (
-    echo [ERRO] Fail at publish.
+  dotnet publish "%PROJ%" -c Release -r win-x64 -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true
+  if errorlevel 1 (
+    echo [ERRO] Failed at Publish.
     pause
     exit /b 1
   )
 )
 
 set "ASPNETCORE_CONTENTROOT=%PUB%"
-start "" /D "%PUB%" "%APP%" --urls=http://localhost:5000"
+start "" /D "%PUB%" "%APP%" --urls=http://localhost:%PORT%
 
 for /l %%i in (1,1,15) do (
-  powershell -NoProfile -Command "try{(New-Object Net.Sockets.TcpClient 'localhost',5000).Dispose();exit 0}catch{exit 1}" >nul
+  powershell -NoProfile -Command "try{(New-Object Net.Sockets.TcpClient 'localhost',%PORT%).Dispose();exit 0}catch{exit 1}" >nul
   if not errorlevel 1 goto :open
   timeout /t 1 >nul
 )
 :open
-start "" "http://localhost:5000"
+start "" "http://localhost:%PORT%"
 endlocal
